d53cd2ab533473adb327da7dcda28509a8341bb7
aretecode <aretecode@gmail.com>
2019-04-18 00:10:46 -0700

    format & fix apollo + profile component render
    - __typeName => typename
    - fix according to prettier
    - use `li` insteadof `<div>`
    - use keys when mapping

--- src/Profile/index.tsx
+++ src/Profile/index.tsx
@@ -3,30 +3,29 @@ import { ApolloProvider, Query } from 'react-apollo'
 import { client, FruitsQuery } from './apollo'
 
 class Profile extends React.Component {
-    static defaultProps = {}
-    render() {
-        return (
-            <ApolloProvider client={client}>
-                <Query query={FruitsQuery}>
-                    {(response: any) => {
-                        console.log(response)
-                        return ''
-                        // const fruitsView = (response.data!.fruits || []).map(
-                        //     (fruit: any) => {
-                        //         return (
-                        //             <>
-                        //                 <div>{fruit.name}</div>
-                        //                 <div>{fruit.brand}</div>
-                        //             </>
-                        //         )
-                        //     }
-                        // )
-                        // return fruitsView
-                    }}
-                </Query>
-            </ApolloProvider>
-        )
-    }
+  static defaultProps = {}
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Query query={FruitsQuery}>
+          {(response: any) => {
+            console.log(response)
+            const fruitsView = (response.data!.fruits || []).map(
+              (fruit: any) => {
+                return (
+                  <li key={fruit.name}>
+                    <p>{fruit.name}</p>
+                    <p>{fruit.brand}</p>
+                  </li>
+                )
+              }
+            )
+            return <ul>{fruitsView}</ul>
+          }}
+        </Query>
+      </ApolloProvider>
+    )
+  }
 }
 
 export { Profile }
